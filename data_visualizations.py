bcf95e48b1a96145d71ac2ffb63b8e03caf8ce87
Hajdu David <hdavid998@gmail.com>
2022-03-03 02:00:18 +0100

    started with matplotlib & pandas libraries

---
+++ data_visualizations.py
@@ -0,0 +1,57 @@
+from genericpath import exists
+import pandas as ps
+import matplotlib.pyplot as mplot
+import numpy as np
+import getopt
+import sys
+
+def usage():
+    print(
+        '-h --help for help\r\n-i --infile filename\r\n'
+    )
+    pass
+
+def opt_walk(opts):
+    verbose = False
+    infile = None
+    for o, a in opts:
+        if o in ("-v", "--verbose"):
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            return None
+        elif o in ("-i", "--infile"):
+            infile = a
+        else:
+            print("unhandled option")
+            usage()
+            return None
+    return { 'verbose':verbose, 'infile':infile }
+
+
+def main():
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:v", ["help", "infile=", "verbose"])
+    except getopt.GetoptError as err:
+        print(err + '\r\n')
+        usage()
+        sys.exit(1)
+    
+    res = opt_walk(opts)
+    verbose = res['verbose']
+    infile = res['infile']
+    
+    if (infile == None) or (not exists(infile)) :
+        print('there is no existing input file\r\nhint: -i, --infile filename')
+        sys.exit(1)    
+    
+    data_set = ps.read_csv(infile, sep=',', squeeze=True, names=['time_stamp', 'acceleration'])
+    arr = data_set.to_numpy()
+    cols = data_set.columns
+    keys = data_set.keys()
+    data_set.plot.line()
+    mplot.show()
+
+if __name__ == '__main__' :
+    main()
