4690e9ed4cdd69c5bd48d693ef969b94ecae1df6
Hajdu David <hdavid998@gmail.com>
2022-03-07 00:41:02 +0100

    some comments added (at line 170)

--- data_visualizations.py
+++ data_visualizations.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+
 from genericpath import exists
 from datetime import datetime
 import pandas as ps
@@ -8,30 +9,28 @@ import numpy as np
 import getopt
 import sys
 
+# this function gives that how to use this program
 def usage() :
     print(
-        '-h --help for help\r\n-i --infile filename\r\n-o --outfile filename'
+        '-h --help for help\r\n-i --infile filename\r\n'
     )
-    pass
 
+# handling of input arguments
+# return with the path of the input file
 def opt_walk(opts) :
     verbose = False
     infile = None
-    outfile = None
     for o, a in opts :
         if o in ("-h", "--help") :
             usage()
-            return None
         elif o in ("-i", "--infile") :
             infile = a
-        elif o in ("-o", "--outfile") :
-            outfile = a
         else :
             print("unhandled option")
             usage()
-            return None
-    return { 'verbose':verbose, 'infile':infile, 'outfile':outfile}
+    return infile
 
+# this function is showing the menu
 def menu() :
     print(
         'Select an option:\r\n'
@@ -46,23 +45,32 @@ def menu() :
         'x) - exit from this program'
     )
 
+# convert datetime strings to milliseconds
+# timestamps parameter must be iterable and the elements must be %Y-%m-%d %H:%M:%S.%f in format
 def convert_timestamps_to_ms(timestamps) :
     ts_list = []
     for ts in timestamps:
         ts_list.append(datetime.strptime(ts, '%Y-%m-%d %H:%M:%S.%f').timestamp() * 1000)
     return ts_list
 
+# this function gives the locations of the data set in a list
 def get_positions_of_data_set(data_set) :
     positions = range(0, len(data_set))
     return positions
 
+# this function finds the peak values of the data_set input parameter
+# data_set must be an two dimensional array
+# it gives back locations of the peak values and of course the peak values with timestamps
 def find_peak_values_with_postitions(data_set) :
     peak_values = [[], []]
     positions = []
+    # if the first data is greater than second, then it is a peak value
     if data_set[0, 1] > data_set[1, 1] :
         peak_values[0].append(data_set[0, 0])
         peak_values[1].append(data_set[0, 1])
         positions.append(0)
+    # this peak finding algorithm is really simple and not the fastest one but it works very well
+    # the algorithm examines the neighbors of the data and if they are less then this, then this data is a peak value
     i = 1
     size = len(data_set[:,0])
     while i < (size - 1) :
@@ -73,12 +81,16 @@ def find_peak_values_with_postitions(data_set) :
             i += 2
         else :
             i += 1
+    # if the last data is greater then penultimate data, then is a peak value
     if data_set[-1, 1] > data_set[-2, 1] :
         peak_values[0].append(data_set[size - 1, 0])
         peak_values[1].append(data_set[size - 1, 1])
         positions.append(size - 1)
     return peak_values, positions
 
+# the goal of this function is to reduce the number of peak values by finding the peaks from the peak_values list (input parameter)
+# if this algorithm runs several times, then the targeted small local ranges will be wider and wider
+# the description of the peak finding algorithm can be find at the find_peak_values_with_positions function
 def reduce_peak_values(peak_values, positions) :
     new_positions = []
     new_peak_values = [[], []]
@@ -102,41 +114,61 @@ def reduce_peak_values(peak_values, positions) :
         new_positions.append(positions[size - 1])
     return new_peak_values, new_positions
     
+# estimating of the sampling period time
+# the problem is that the dates in the input files are not accurate enough
+# to solve this problem I created a really simple algorithm in this function
 def calculate_sample_time(ts_list) :
     step = 1
     finished = False
     sample_time = 0
+    n = len(ts_list)
     ctr = 1
-
+    # if finished parameter will be true, then the algorithm probably estimated the value of the sampling period time
+    # if the finished parameter becomes true, but the algorithm failed, then the return value will be zero
     while not finished :
+        # start with step=1 (this step or jump value will be greater and greater during the process)
+        # calculation of the time difference between two pairs of values and counting the operations
         pre_dif = ts_list[step] - ts_list[0]
-        dif = ts_list[step + step] - ts_list[step]
+        dif = ts_list[2 * step] - ts_list[step]
         ctr += 1
+        # if the two time differences are not equal to each other, then the step value will be increased
         if pre_dif != dif :
             step += 1
-        elif ctr > len(ts_list) :
+        # if the count of the operations is greater than length of the input list, then algorithm failed or there is no valid sampling frequency value
+        elif ctr > n :
             finished = True
+        # if the compared time differences are equal to each other, then this algorithm makes sure that this sampling period value is acceptable
         else :
+            # the stopped flag will be true, if the calculated time difference is not acceptable as sampling period
             stopped = False
-            for i in range(3 * step, 3000, step) :
+            # the algorithm assumes that there is a lot of data in the input file, so it checks only 1% of the data set
+            for i in range(3 * step, int(n * 0.01), step) :
                 dif = ts_list[i] - ts_list[i - step]
                 if dif != pre_dif:
                     stopped = True
                     break
+            # if stopped flag is true, then the algorithm try to estimate the sampling period with increased step value
             if stopped :
                 step += 1
+            # if the stopped flag is false, then the algorithm estimated the value of the period time
+            # (attention! we use only 1% of data, so the success of this algorithm depends on the size of data)
             else :
                 sample_time = (ts_list[step] - ts_list[0]) / step
                 finished = True
-
+    # get the sample time in sec (ts_list is in ms)
     return sample_time / 1000
 
 def show_statistics(data_set) :
+    # mean - 1/N * sum(data)
     mean = stat.fmean(data_set)
+    # median - ex.: [1, 2, 3, 4, 5] -> 3
     median = stat.median(data_set)
+    # mode - ex.: [1, 1, 2, 3, 3, 4, 4, 4, 4, 5, 5] -> 4
     mode = stat.mode(data_set)
-    variance = stat.pvariance(data_set)
-    deviation = stat.pstdev(data_set)
+    # deviation - sqrt(VAR(data))
+    deviation = stat.pstdev(data_set, mean)
+    # variance - VAR(data) = 1/N * sum([(d - mean)**2 for d in data])
+    variance = deviation**2
     print(
         'Mean of data set: ' + str(mean) + '\r\n'
         'Mode of data set: ' + str(mode) + '\r\n'
@@ -164,8 +196,13 @@ def show_fft_figure(freq_line, data_set, sampling_frequency) :
     mplot.grid()
     mplot.show()
 
-def show_scatter() :
-    return None
+def show_scatter(data_set) :
+    x = range(0, len(data_set))
+    mplot.scatter(x, data_set)
+    mplot.title('Scatter diagram')
+    mplot.xlabel('Locations')
+    mplot.ylabel('Values')
+    mplot.show()
 
 def get_user_input():
     ok = False
@@ -227,15 +264,10 @@ def main() :
     
     res = opt_walk(opts)
     infile = res['infile']
-    outfile = res['outfile']
     
     if (infile == None) or (not exists(infile)) :
         print('there is no existing input file\r\nhint: -i, --infile filename')
         sys.exit(1)    
-
-    if (outfile == None) or (not exists(outfile)) :
-        outfile = './statistical_informations.txt'
-        print('There is no existing output file\r\nDefault output file\'s name is statistical_informations.txt. You can find it the working folder.')
 
     data_set = ps.read_csv(infile, sep=',', squeeze=True, names=['time_stamp', 'acceleration'])
     arr = data_set.to_numpy()
@@ -270,7 +302,7 @@ def main() :
         elif option == '5' :
             show_histogram(arr[:,1], sampling_time_sec)
         elif option == '6' :
-            show_scatter()
+            show_scatter(arr[:,1])
         elif option == '7' :
             show_statistics(arr[:,1])
         elif option == 'x' :
